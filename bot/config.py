94e52168bd0c2c97e9610e313f2bb9b7ca572625
fpband <82159763+fpband@users.noreply.github.com>
2022-06-16 15:25:19 +0430

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -37,7 +37,7 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "🟢")
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▓")
     UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "⚪️")
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
       # because, https://t.me/c/1494623325/5603
